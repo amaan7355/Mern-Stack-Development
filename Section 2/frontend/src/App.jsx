ebd679a35159d4189d04ee9609fd5e99edb4684d
amaan7355 <alamamaan334@gmail.com>
2023-08-02 20:02:10 +0530

    react

---
+++ Section 2/frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
+
+function App() {
+  return (
+    <div className="">
+      <h1>My React App</h1>
+      <Home/>
+      <Login/>
+      <SignUp/>
+    </div>
+  );
+}
+
+export default App;
